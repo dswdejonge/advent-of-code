a26e7ddd8c5a49afac1b334074294653b232ae27
dswdejonge <dsw.dejonge@gmail.com>
2019-12-15 18:01:39 +0100

    Add part 2, tailed recursion.

--- aoc1.R
+++ aoc1.R
@@ -9,3 +9,16 @@ fuel_per_module <- function(mass){
 
 # sum to get total mass
 fuel <- sum(fuel_per_module(masses))
+
+# take into account mass of fuel itself
+fuel_equation <- function(mass){
+  fuel <- floor(mass / 3) - 2
+  fuel[which(fuel < 0)] <- 0
+  if(all(fuel == 0)){
+    return(fuel)
+  } else {
+    return(fuel + fuel_equation(fuel))
+  }
+}
+
+fuel2 <- sum(fuel_equation(masses))
