6904a9cd5db7b94cf2f0d2fe211ee46d2eef05f2
dswdejonge <dsw.dejonge@gmail.com>
2020-12-01 21:07:44 +0000

    Part 1 complete.

--- 2020_day1.R
+++ 2020_day1.R
@@ -1,15 +1,19 @@
 library(testthat)
 
-findTermsToSum <- function(A, B, result){
-  complement <- resSum - A
-  i <- which(complement %in% B)
-  return(c(A[i], complement[i]))
+### PART 1 ###
+findTermsToSum <- function(num_vec, resulting_sum){
+  complement <- resulting_sum - num_vec
+  return(num_vec[which(complement %in% num_vec)])
 }
 
-A <- c(200, 300, 400)
-B <- c(750, 800, 200)
-resSum <- 1000
+num_vec <- c(200, 300, 400, 700)
+resulting_sum <- 1000
 
 test_that("function finds correct terms to sum",{
-  expect_equal(sum(findTermsToSum(vecA, vecB, resSum)), resSum)
+  expect_equal(sum(findTermsToSum(num_vec, resulting_sum)), resulting_sum)
 })
+
+input <- as.numeric(readLines("2020_day1_input.txt"))
+resulting_sum <- 2020
+result <- findTermsToSum(input, resulting_sum)
+answer <- prod(result)
